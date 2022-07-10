de087f4357622587b8a8eb43a2e6dd3a9436ac9f
Artem Vasenin <a.vasenin@neurotrade.ru>
2022-07-10 17:20:01 +0300

    feat(logging.py): Add context manger to temporarily change logging level

--- nt_utils/logging.py
+++ nt_utils/logging.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import logging
 import sys
 import traceback
 import warnings
+from collections import defaultdict
 
 ORIGINAL_SHOWWARNINGS = warnings.showwarning
 
@@ -14,9 +16,35 @@ def _warn_with_traceback(message, category, filename, lineno, file=None, line=No
 
 
 class warn_with_traceback:
-    def __enter__(self):
+    def __enter__(self) -> None:
         self._orig_show = warnings.showwarning
         warnings.showwarning = _warn_with_traceback
 
     def __exit__(self, *_):
         warnings.showwarning = ORIGINAL_SHOWWARNINGS
+
+
+class change_root_level:
+    def __init__(self, level: int, skip_handlers=False):
+        self._level = level
+        self._skip_handlers = skip_handlers
+        self._original_level = None
+        self._original_handler_levels = None
+
+    def _update_levels(self, root_level: int, handler_levels=None):
+        root_logger = logging.getLogger()
+        root_logger.setLevel(root_level)
+        if self._skip_handlers:
+            return
+        handler_levels = handler_levels or self._original_handler_levels or defaultdict(lambda: root_level)
+        for handler in root_logger.handlers:
+            handler.setLevel(handler_levels[handler])
+
+    def __enter__(self) -> None:
+        root_logger = logging.getLogger()
+        self._original_level = root_logger.level
+        self._original_handler_levels = {handler: handler.level for handler in root_logger.handlers}
+        self._update_levels(self._level)
+
+    def __exit__(self, *_):
+        self._update_levels(self._original_level, self._original_handler_levels)
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
+import logging
 import warnings
 
 import pytest
 
-from nt_utils.logging import warn_with_traceback
+from nt_utils.logging import change_root_level, warn_with_traceback
 
 
 def test_warnings_traceback(capsys):
@@ -19,3 +20,16 @@ def test_warnings_traceback(capsys):
     captured = capsys.readouterr()
     assert captured.out == ""
     assert captured.err == ""
+
+
+def test_change_root_level(caplog):
+    caplog.set_level(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+    logger.info("before")
+    with change_root_level(logging.WARNING):
+        logger.info("inside")
+    logger.info("after")
+    assert "before" in caplog.text
+    assert "inside" not in caplog.text
+    assert "after" in caplog.text
