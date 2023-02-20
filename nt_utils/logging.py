b39ab9eb58009100d1422bdfae4e2653354d0c67
Artem Vasenin <vasart169@gmail.com>
2023-02-20 23:47:05 +0000

    feat(logging.py): Add setup_logging from ntf

--- nt_utils/logging.py
+++ nt_utils/logging.py
@@ -4,6 +4,11 @@ import logging
 import sys
 import traceback
 import warnings
+from pathlib import Path
+from traceback import format_exception
+from typing import Any
+
+from coloredlogs import DEFAULT_FIELD_STYLES, DEFAULT_LEVEL_STYLES, ColoredFormatter
 
 ORIGINAL_SHOWWARNINGS = warnings.showwarning
 
@@ -23,6 +28,12 @@ class warn_with_traceback:
         warnings.showwarning = ORIGINAL_SHOWWARNINGS
 
 
+def assert_with_logging(status: bool, message: str):
+    if not status:
+        logging.critical(message)
+    assert status, message
+
+
 class change_logging_level:
     def __init__(self, level: int):
         self._level = level
@@ -36,3 +47,92 @@ class change_logging_level:
     def __exit__(self, *_):
         logging.getLogger().setLevel(self._original_level)
         self._original_level = None
+
+
+def offset_logger_level(logger: logging.Logger, amount: int):
+    def change_level(record: logging.LogRecord):
+        record.levelno += amount
+        record.levelname = logging.getLevelName(record.levelno)
+        return record
+
+    logger.addFilter(change_level)
+
+
+def get_file_handler(log_file: Path, logging_level: int | str = logging.DEBUG, append=True, use_year=False):
+    file_handler = logging.FileHandler(log_file, mode="a" if append else "w")
+    formatter = get_logging_formatter(use_year=use_year)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging_level)
+    return file_handler
+
+
+StylesType = dict[str, dict[str, Any]]
+CUSTOM_LEVEL_STYLES: StylesType = {
+    **DEFAULT_LEVEL_STYLES,
+    "debug": {"color": "black", "bright": True},
+    "info": {"faint": True},
+    "warning": {"color": "yellow", "bold": True},
+    "error": {"color": "red", "bold": True},
+    "critical": {"color": "magenta", "bold": True, "underline": True},
+}
+CUSTOM_FIELD_STYLES: StylesType = {**DEFAULT_FIELD_STYLES, "levelname": {"color": "white"}}
+CUSTOM_FORMAT = "{asctime:^} [{levelname:^8s}]({name:>30s}+{lineno:>4d}): {message:<s}"
+
+
+def get_logging_formatter(
+    color=False, level_styles_update: StylesType = None, field_styles_update: StylesType = None, use_year=False
+):
+    fmt = CUSTOM_FORMAT
+    maybe_year = "%Y-" if use_year else ""
+    date_fmt = f"{maybe_year}%m-%d %H:%M:%S"
+    if not color:
+        return logging.Formatter(fmt, style="{", datefmt=date_fmt)
+    level_styles = {**CUSTOM_LEVEL_STYLES, **(level_styles_update or {})}
+    field_styles = {**CUSTOM_FIELD_STYLES, **(field_styles_update or {})}
+    return ColoredFormatter(fmt, style="{", datefmt=date_fmt, level_styles=level_styles, field_styles=field_styles)
+
+
+def excepthook(*args):
+    logging.error("Uncaught exception:\n" + "".join(format_exception(*args)))
+
+
+def setup_logging(
+    log_file: Path = None,
+    print_level: int | str = logging.INFO,
+    append=False,
+    level_styles_update: dict[str, dict[str, Any]] = {},
+    field_styles_update: dict[str, dict[str, Any]] = {},
+    disable_colors=False,
+    log_uncaught_exceptions=True,
+    use_year=False,
+):
+    """
+    Make logging nice
+
+    :param log_file: in which file to write the log
+    :param print_level: Level of logging to stdout
+    :param append: Append to log file instead of overwriting
+    :param level_styles_update: Update to color styles for stdout formatter
+    :param disable_colors: Disable colors in stdout
+    :param log_uncaught_exceptions: Add uncaught exceptions to the log, not guaranteed
+    :param use_year: Add year to logs
+    """
+    if isinstance(print_level, str):
+        print_level = int(print_level) if print_level.isdigit() else getattr(logging, print_level)
+    formatter = get_logging_formatter(use_year=use_year)
+    colored_formatter = get_logging_formatter(
+        color=True, level_styles_update=level_styles_update, field_styles_update=field_styles_update, use_year=use_year
+    )
+
+    handlers = []
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter if disable_colors else colored_formatter)
+    console_handler.setLevel(print_level)
+    handlers.append(console_handler)
+    if log_file is not None:
+        handlers.append(get_file_handler(log_file, append=append, use_year=use_year))
+
+    logging.basicConfig(level=logging.DEBUG, handlers=handlers)
+
+    if log_uncaught_exceptions:
+        sys.excepthook = excepthook
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 import logging
 import warnings
+from pathlib import Path
 
 import pytest
 
 from nt_utils import warn_with_traceback
-from nt_utils.logging import change_logging_level
+from nt_utils.logging import assert_with_logging, change_logging_level, offset_logger_level, setup_logging
 
 
 def test_warnings_traceback(capsys):
@@ -34,3 +35,47 @@ def test_change_logging_level(caplog):
     assert "before" in caplog.text
     assert "inside" not in caplog.text
     assert "after" in caplog.text
+
+
+def test_assert_with_logging(caplog):
+    assert_with_logging(True, "bar")
+    with pytest.raises(AssertionError):
+        assert_with_logging(False, "foo")
+    assert caplog.record_tuples == [("root", logging.CRITICAL, "foo")]
+
+
+@pytest.mark.parametrize(
+    ("amount", "expected"),
+    [
+        (0, [("test_logger", 20, "foo"), ("test_logger", 20, "bar")]),
+        (10, [("test_logger", 20, "foo"), ("test_logger", 30, "bar")]),
+    ],
+)
+def test_offset_logger_level(amount, expected, caplog):
+    caplog.set_level(logging.DEBUG)
+    logger = logging.getLogger("test_logger")
+    logger.log(logging.INFO, "foo")
+    offset_logger_level(logger, amount)
+    logger.log(logging.INFO, "bar")
+
+    assert caplog.record_tuples == expected
+
+
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {},
+        {
+            "log_file": Path("foo"),
+            "print_level": "INFO",
+            "append": True,
+            "level_styles_update": {"warning": {"color": "red"}},
+            "field_styles_update": {"levelname": {"color": "yellow"}},
+            "disable_colors": True,
+        },
+    ],
+)
+def test_setup_logging(kwargs, tmp_path):
+    if "log_file" in kwargs:
+        kwargs["log_file"] = tmp_path / kwargs["log_file"]
+    setup_logging(**kwargs)
